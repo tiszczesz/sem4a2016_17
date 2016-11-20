a7b74fd50cb9f952360c178c88160ecd0a4945e7
cosinus <cosinus@cosinuskrakow>
2016-11-20 12:47:34 +0100

    first 1

--- cw13/addWorker.php
+++ cw13/addWorker.php
@@ -1,18 +1,19 @@
 <?php
 
-if(isset($_POST['imie']) && isset($_POST['nazwisko'])){
+if (isset($_POST['imie']) && isset($_POST['nazwisko'])) {
     $imie = trim($_POST['imie']);
     $nazwisko = trim($_POST['nazwisko']);
-    if(is_numeric($_POST['pensja'])){
+    if (is_numeric($_POST['pensja'])) {
         $pensja = intval($_POST['pensja']);
-    }else{
+    } else {
         header("Location: dodaj.html");
     }
-    if($imie==="" || $nazwisko===""){
+    if ($imie == "" || $nazwisko == "") {
         header("Location: dodaj.html");
+    } else {
+        require_once 'functions.php';
+        AddNewWorker("dane.txt", $imie, $nazwisko, $pensja);
+        header("Location: cw13.php");
     }
-    require_once 'functions.php';
-    AddNewWorker("dane.txt",$imie,$nazwisko,$pensja);
-    header("Location: cw13.php");
 }
 
