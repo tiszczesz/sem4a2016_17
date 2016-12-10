f9d2f681f438da3d1f292e829d9acde8a997e11a
cosinus <cosinus@cosinuskrakow>
2016-12-10 14:04:05 +0100

    first 1

---
+++ cw14/sql/pracownicy_sem4a.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 10 Gru 2016, 14:00
+-- Wersja serwera: 10.1.9-MariaDB
+-- Wersja PHP: 5.5.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `pracownicy_sem4a`
+--
+CREATE DATABASE IF NOT EXISTS `pracownicy_sem4a` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE `pracownicy_sem4a`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pracownicy`
+--
+
+DROP TABLE IF EXISTS `pracownicy`;
+CREATE TABLE `pracownicy` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `pensja` decimal(10,2) NOT NULL,
+  `stanowiskoid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `pracownicy`
+--
+
+INSERT INTO `pracownicy` (`id`, `imie`, `nazwisko`, `pensja`, `stanowiskoid`) VALUES
+(1, 'Adam', 'Nowak', '5600.00', 1),
+(2, 'Maria', 'Nowicka', '3500.00', 3),
+(3, 'Mariusz', 'Frytrk', '2800.00', 4),
+(4, 'Mateusz', 'Jurecki', '3200.00', 4),
+(5, 'Anna', 'Wawecka', '4500.00', 2),
+(6, 'Franciszek', 'Krypek', '3350.00', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `stanowiska`
+--
+
+DROP TABLE IF EXISTS `stanowiska`;
+CREATE TABLE `stanowiska` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `opis` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `stanowiska`
+--
+
+INSERT INTO `stanowiska` (`id`, `nazwa`, `opis`) VALUES
+(1, 'kierownik', 'kieruje ludźmi'),
+(2, 'sprzedawca', 'sprzedaje towar'),
+(3, 'sekretarka', 'wiadomo co'),
+(4, 'kierowca', 'kieruje samochodem');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `stanowiskoid` (`stanowiskoid`);
+
+--
+-- Indexes for table `stanowiska`
+--
+ALTER TABLE `stanowiska`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT dla tabeli `stanowiska`
+--
+ALTER TABLE `stanowiska`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `pracownicy`
+--
+ALTER TABLE `pracownicy`
+  ADD CONSTRAINT `prac_stan` FOREIGN KEY (`stanowiskoid`) REFERENCES `stanowiska` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
